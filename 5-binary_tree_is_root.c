456f0ea438e5dd9a05999d2898816c9ecb4eb1a4
Jean-Luc <jlucbilo@gmail.com>
2023-09-12 15:38:14 +0200

    adding the files of the tasks 4 and 5: binay tree is leaf and binary tree is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - a function that checks if a given node is a root
+ * @node: the node that will be check
+ * Return: o for success
+ **/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	return (0);
+}
